a5dd7b9bfe5969d1845f3e0b6b865a9b68f94576
maigen skjelstad & Tara Nelson <student@epicodus.com>
2014-04-14 11:51:22 -0700

    add question creation controller and question view page

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: "Question Saved!"
+    else
+      render root_path, notice: "Please check your entry and try again."
+    end
+  end
+
+  private
+  def question_params
+    params.require(:question).permit(:content, :user_id)
+  end
+end
