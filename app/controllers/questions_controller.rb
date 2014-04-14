0d0592e771e1b419eac1e7dd59f1b43b1ccf0aae
maigen skjelstad & Tara Nelson <student@epicodus.com>
2014-04-14 12:21:42 -0700

    add new questions and link to page that displays question and answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,6 +12,10 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
   private
   def question_params
     params.require(:question).permit(:content, :user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 StackClone::Application.routes.draw do
   devise_for :users
+  resources :questions do
+    resources :answers
+  end
+
   root 'questions#index'
 
 
